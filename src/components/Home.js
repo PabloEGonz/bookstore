891f0e6c4189d03993b09542ff5c69939b61d2c2
Pablo <ezekielgonzalezr@gmail.com>
2023-06-08 19:51:58 -0600

    Create home section

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Books from './Books'
+import Form from './Form'
+
+export default function () {
+    return (
+        <div>
+            <Books />
+            <Form />
+        </div>
+    )
+}
