f2634e899d4e76b9d2335674ec28624198b3a75d
Pablo <ezekielgonzalezr@gmail.com>
2023-06-08 19:26:55 -0600

    Create error page

---
+++ src/components/Errorpage.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Errorpage() {
+  return (
+    <div>
+        <h1>Opps! Sorry, something wrong has occured</h1>
+        <h2>Please go back!</h2>
+    </div>
+  )
+}
