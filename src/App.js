5e4dc7406f5b85e9f127e9093dab269c45aa98f7
Pablo <ezekielgonzalezr@gmail.com>
2023-06-08 19:25:13 -0600

    Add app section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,41 @@
 import './App.css';
+import { createBrowserRouter, createRoutesFromElements, RouterProvider, Route, Link, Outlet } from 'react-router-dom';
+import Books from './components/Books';
+import Categories from './components/Categories';
+import Errorpage from './components/Errorpage';
 
 function App() {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<Root />} >
+        <Route index element={<Books />}></Route>
+        <Route path="/categories" element={<Categories />}></Route>
+        <Route path="*" element={<Errorpage />}></Route>
+      </Route>
+    ))
   return (
     <div className="App">
+
+      <RouterProvider router={router} />
     </div>
   );
+}
+
+const Root = () => {
+  return (
+    <>
+      <div>
+        <h1>Bookstore CMS</h1>
+        <ul>
+          <li><Link to="/">Books</Link></li>
+          <li><Link to="/categories">Categories</Link></li>
+        </ul>
+      </div>
+      <div>
+        <Outlet />
+      </div>
+    </>
+  )
 }
 
 export default App;
