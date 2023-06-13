bda19198f77f80eba5706ae383f75f1986eb3071
Pablo <ezekielgonzalezr@gmail.com>
2023-06-13 12:02:27 -0600

    Add id for delete button

--- src/components/Books.js
+++ src/components/Books.js
@@ -10,6 +10,7 @@ export default function Books() {
         {books.map((book) => (
           <Book
             key={book.item_id}
+            id={book.item_id}
             title={book.title}
             author={book.author}
             category={book.category}
