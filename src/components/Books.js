deaa7ed4d28497bcd812227efd6476acf3333ebf
Pablo <ezekielgonzalezr@gmail.com>
2023-06-14 21:53:17 -0600

    Add use effect to call api

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,9 +1,31 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import Book from './Book';
+import { getBooks } from '../redux/books/booksSlice';
 
 export default function Books() {
-  const { books } = useSelector((state) => state.books);
+  const dispatch = useDispatch();
+  const { books, isLoading, error } = useSelector((state) => state.books);
+
+  useEffect(() => {
+    dispatch(getBooks());
+  }, [dispatch]);
+  if (isLoading) {
+    return (
+      <div>
+        <h1>Loading</h1>
+      </div>
+    );
+  } if (error) {
+    return (
+      <div>
+        <h1>
+          Sorry, something went wrong. Error:
+          {error}
+        </h1>
+      </div>
+    );
+  }
   return (
     <div>
       <ul className="flex-cl list-ctnr">
@@ -18,6 +40,5 @@ export default function Books() {
         ))}
       </ul>
     </div>
-
   );
 }
