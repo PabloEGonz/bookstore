6889022482b6cdff74e706366b06950e62712ba2
Pablo <ezekielgonzalezr@gmail.com>
2023-06-15 07:25:40 -0600

    Add submit new book async

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,51 +1,71 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { v4 as uuidv4 } from 'uuid';
-import { useDispatch } from 'react-redux';
-import { addBook } from '../redux/books/booksSlice';
+import { useDispatch, useSelector } from 'react-redux';
+import { addBooks } from '../redux/books/booksSlice';
 
 export default function Form() {
-  const [error, setError] = useState('');
-  const dispatch = useDispatch();
-  const [obj, setObj] = useState({
+  const empty = {
     item_id: uuidv4(),
     title: '',
     author: '',
     category: '',
-  });
-  const handleChange = (e) => {
-    e.preventDefault();
-    if (e.target.id === 'title') {
-      setObj({
-        ...obj,
-        title: e.target.value,
-      });
-    } else if (e.target.id === 'author') {
-      setObj({
-        ...obj,
-        author: e.target.value,
-      });
-    } else if (e.target.id === 'category') {
-      setObj({
-        ...obj,
-        category: e.target.value,
-      });
-    }
   };
+  const { formMessage } = useSelector((state) => state.books);
+  const [error, setError] = useState('');
+  const dispatch = useDispatch();
+  const [obj, setObj] = useState(empty);
+
+  useEffect(() => {
+    setError(formMessage);
+  }, [formMessage]);
+
+  const handleTitlChng = (e) => {
+    e.preventDefault();
+    setObj({
+      ...obj,
+      title: e.target.value,
+    });
+  };
+
+  const handleAuthChng = (e) => {
+    setObj({
+      ...obj,
+      author: e.target.value,
+    });
+  };
+
+  const handleCatChng = (e) => {
+    setObj({
+      ...obj,
+      category: e.target.value,
+    });
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
     if (obj.title.trim() && obj.author.trim() && obj.category.trim()) {
-      dispatch(addBook(obj));
+      dispatch(addBooks(obj));
+      setObj({
+        ...empty,
+        category: obj.category,
+      });
+      setTimeout(() => {
+        setError('');
+      }, 4000);
     } else {
       setError('Please make sure you have fill out all the inputs.');
+      setTimeout(() => {
+        setError('');
+      }, 4000);
     }
   };
   return (
     <div className="add">
       <h2>ADD NEW BOOK</h2>
-      <form className="flex" action="post" onChange={handleChange}>
-        <input id="title" type="text" placeholder="Book Title" value={obj.title} />
-        <input id="author" type="text" placeholder="Book Author" value={obj.author} />
-        <select id="category">
+      <form className="flex" action="post">
+        <input id="title" type="text" placeholder="Book Title" value={obj.title} onChange={handleTitlChng} />
+        <input id="author" type="text" placeholder="Book Author" value={obj.author} onChange={handleAuthChng} />
+        <select id="category" onChange={handleCatChng}>
           <option value="">--Category--</option>
           <option value="Fantasy">Fantasy</option>
           <option value="Horror">Horror</option>
